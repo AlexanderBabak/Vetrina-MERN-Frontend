b83022acaa14ab47f2f2b09735a4035a964ba1fa
Alexander Babak <alexander.babak@lastingdynamics.com>
2023-07-09 11:34:54 +0300

    how to fix ssr warning

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,16 @@ import { Navigation } from "./navigation/Navigation";
 import { store } from "./redux/store";
 
 function App() {
+  //to remove the server rendering related warning, you need to update the native base version or fix the code for this library
+
+  //Instead of ignoring the error, you can resolve it by yourself. Here's how:
+  // Navigate to node_modules/native-base/src/core/NativeBaseProvider.tsx
+  // Delete <SSRProvider></SSRProvider> that wraps {children}. Take care not to delete {children}.
+  // Remove SSRProvider import. That is, delete this line import { SSRProvider } from '@react-native-aria/utils';
+  // Run npx patch-package native-base. Select yes in the prompt.
+  // When Native Base officially fixes it, you can delete the patch from the patch directory that was created and reinstall native-base
+  // https://github.com/GeekyAnts/NativeBase/issues/5758#issuecomment-1627636324
+
   return (
     <>
       <SafeAreaView style={{ flex: 1 }} edges={["top"]}>
